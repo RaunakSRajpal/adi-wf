235c5905405cf7582a315c114ad07bb286b1f211
RaunakSRajpal <raunaks.rajpal002@gmail.com>
2025-08-22 12:59:33 -0400

    bug-fixes

--- scripts/init-adi-hdl.sh
+++ scripts/init-adi-hdl.sh
@@ -6,20 +6,26 @@ ADI_DIR="$WS/adi-hdl-1"
 HDL_DIR="$ADI_DIR/hdl"
 XVERSION="2024.2"
 
-
-EVAL_BOARD=${1:-"adrv9361z7035"}
-CARRIER=${2:-"ccbob_cmos"}
-XSA_FILE="$HDL_DIR/projects/$EVAL_BOARD/$CARRIER/system_top.xsa"
+proj_name="$1"
+EVAL_BD=${2:-"adrv9361z7035"}
+CARRIER=${3:-"ccbob_cmos"}
+hdl_branch=${4:-"main"}
+XSA_FILE="$HDL_DIR/projects/$EVAL_BD/$CARRIER/system_top.xsa"
 UBOOT_FILE="$ADI_DIR/prep-BOOT/u-boot_zynq_adrv9361.elf"
 
 BOOT_DIR="$ADI_DIR/prep-BOOT"
-LOGFILE=$HDL_DIR/setup-HDL.log
+LOGFILE=$ADI_DIR/setup-HDL.log
 BUILD_DIR="/$ADI_DIR/build_boot_bin"
 OUTPUT_DIR="$ADI_DIR/output_boot_bin"
 
-error() {
+usage() {
     echo -e "[ERROR]\t$1\n" | tee -a $LOGFILE >&2
-    echo -e "\t\t usage: $0 \'eval_bd\' \'carrier_bd\' \n" | tee -a $LOGFILE 2>&1
+    echo -e "\t\t usage: $0 <eval_bd> <carrier_bd> \n" | tee -a $LOGFILE 2>&1
+    exit 1
+}
+
+error() {
+    echo -e "[ERROR]\t$1\n" | tee -a $LOGFILE 2>&1
     exit 1
 }
 
@@ -29,36 +35,39 @@ status() {
 
 
 ## Test for arguments
-    ### Test if HDL directory is present,, otherwise clone repo
-    mkdir -p $ADI_DIR/
+    [ -n $proj_name ] || \
+            usage "Project name not specified"
+    
+    [ -n $EVAL_BD ] || \
+     		usage "ADI Evaluation board not specified"
+    
+    [ -n $CARRIER ] || \
+    		usage "ADI Carrier board not specified"
+    
+    mkdir -p $ADI_DIR/ && cd $ADI_DIR/
+    touch $LOGFILE && \
+        echo -e "Initialize ADI-HDL repository.\n \
+             Setup required directories and envirnment variables\n" > $LOGFILE
+    
+    ### Test if HDL directory is present,, otherwise clone repo    
     [ -d "$ADI_DIR/hdl" ] || {
             status "ADI hdl directory not found... installing from \'analogdevicesinc\' repository"
             git clone https://github.com/analogdevicesinc/hdl.git \
-                    --no-single-branch --depth=10 \
+                    --no-single-branch \
                     -- hdl | tee -a $LOGFILE 2>&1
             status "analogdevicesinc/hdl repository cloned"
+            git checkout $hdl_branch
     }
     
-    cd $HDL_DIR
-    touch $LOGFILE
-    
-    if [ -z $EVAL_BOARD ]; then
-     		error "ADI Evaluation board not specified"
-    else
-       [ -d "$HDL_DIR/projects/$EVAL_BOARD" ] || \
+    [ -d "$HDL_DIR/projects/$EVAL_BD" ] || \
     		error "Evaluation board not found"
-    fi
-     
-    if [ -z $CARRIER ]; then
-    		error "ADI Carrier board not specified"
-    else
-       [ -d "$HDL_DIR/projects/$EVAL_BOARD/$CARRIER" ] || \
+    
+    [ -d "$HDL_DIR/projects/$EVAL_BD/$CARRIER" ] || \
     		error "Carrier board not found"
-    fi
     
 
 ## Setup environment
-    cd $AD_DIR
+    cd $ADI_DIR
     source /opt/Xilinx/Vivado/$XVERSION/settings64.sh
 
     export PATH=$PATH:/opt/Xilinx/Vivado/$XVERSION/bin:/opt/Xilinx/Vitis/$XVERSION/bin
@@ -72,8 +81,23 @@ status() {
 ## Build HDL project 
     # export ADI_IGNORE_VERSION_CHECK=1
     (
-        cd $HDL_DIR/projects/$EVAL_BOARD/$CARRIER/ && \
-        make -j5
-        status "Project $EVAL_BOARD-$CARRIER: build complete"
+        cd $HDL_DIR/projects/$EVAL_BD/$CARRIER/ && \
+        make -j5 | tee -a $LOGFILE 2>&1
+        status "Project $EVAL_BD-$CARRIER: build complete"
+        
+        cd ../../
+        mkdir -p $proj_name
+        cp -r $EVAL_BD ./$proj_name        
     )
+    
+## Build ADI IP cores
+    if [ "$EVAL_BD" == "library" ] ; then
+    (
+        cd $HDL_DIR/ && \
+        make -C lbrary all | tee -a $LOGFILE 2>&1
+        status "$HDL_DIR/library: build complete"
+        ##TODO --
+        #--- add library directory to Vivado IP repository
+    )
+    
 
