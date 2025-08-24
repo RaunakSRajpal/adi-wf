7232168ad08812532c5b560b5db754ff5da8094d
RaunakSRajpal <raunaks.rajpal002@gmail.com>
2025-08-23 20:09:38 -0400

    gpio reg ops

--- drivers/gpio/gpio.c
+++ drivers/gpio/gpio.c
@@ -18,11 +18,11 @@ MODULE_VERSION("1.0");
 
 /* Global variables/defines */
 //----TODO----//
-#define XGPIOPS_BASE_ADDR   (uint32_t*)0xE000A000
-#define XGPIOPS_DATA_(X)    (uint32_t*)0x00000040 + (X*4)
-#define XGPIOPS_DATA_RO_(X) (uint32_t*)0x00000060 + (X*4)
-#define XGPIOPS_DIRM_(X)    (uint32_t*)0x00000204 + (X*4 << 1)
-#define XGPIOPS_OEN_(X)     (uint32_t*)0x00000208 + (X*4 << 1)
+#define XGPIOPS_BASE_ADDR       (uint32_t*)0xE000A000
+#define XGPIOPS_DATA__(X)       (uint32_t*)0x00000040 + (X*4)
+#define XGPIOPS_DATA_RO__(X)    (uint32_t*)0x00000060 + (X*4)
+#define XGPIOPS_DIRM__(X)       (uint32_t*)0x00000204 + (X*4 << 1)
+#define XGPIOPS_OEN__(X)        (uint32_t*)0x00000208 + (X*4 << 1)
 
 static unsigned int *gpio_registers = NULL;
 static struct proc_dir_entry *gpio_proc = NULL;
@@ -52,17 +52,25 @@ static const struct proc_ops gpio_ops = {
 
 /* ----------------- Function definitions ----------------- */
 
-static int gpio_on(unsigned int pin) {
-    unsigned int *dirm_2 = gpio_registers + (uint32_t*)XGPIOPS_DIRM_(2);
-    unsigned int *data_2 = gpio_registers + (uint32_t*)XGPIOPS_DATA_RO_(2);
-    // TODO -- REG OPS
-    *dirm_2 |= (1 << pin);
-    *data_2 
+static int gpio_on(unsigned int bank, unsigned int pin) {
+    unsigned int *dirm_x = gpio_registers + (uint32_t*)XGPIOPS_DIRM__(bank);
+    unsigned int *oen_x  = gpio_registers + (uint32_t*)XGPIOPS_OEN__(bank);
+    unsigned int *data_x = gpio_registers + (uint32_t*)XGPIOPS_DATA_RO__(bank);
+    // REG - OPS
+    *dirm_x |= (1 << pin);
+    *oen_x |= (1 << pin);
+    *data_x |= (1 << pin);
     return 0;
 }
 
-static int gpio_off(unsigned int pin) {
-    // TODO -- REG OPS
+static int gpio_off(unsigned int bank, unsigned int pin) {
+    unsigned int *dirm_x = gpio_registers + (uint32_t*)XGPIOPS_DIRM__(bank);
+    unsigned int *oen_x  = gpio_registers + (uint32_t*)XGPIOPS_OEN__(bank);
+    unsigned int *data_x = gpio_registers + (uint32_t*)XGPIOPS_DATA_RO__(bank);
+    // REG - OPS
+    *dirm_x |= (1 << pin);
+    *oen_x |= (1 << pin);
+    *data_x &= ~(1 << pin);
     return 0;
 }
 
@@ -75,7 +83,7 @@ static ssize_t gpio_read(struct file *file, char __user *devbuf, size_t buf_size
 static ssize_t gpio_write(struct file *file, const char __user *devbuf, size_t buf_size, loff_t *offset) {
     printk("GPIO_XOR: Writing to Device\n");
     int ret;
-    ret = copy_from_user(databuf, devbuf, 8);
+    ret = copy_from_user(databuf, devbuf, buf_size);
     if (ret)
         return 0;
         
