7e49fc98a4b84664b626911dadd4ad4294c4eef6
RaunakSRajpal <raunaks.rajpal002@gmail.com>
2025-09-04 13:25:51 -0400

    fix usr_test.c

--- drivers/usr_test_gpio.c
+++ drivers/usr_test_gpio.c
@@ -6,23 +6,26 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void xgpio_readreg(int file_desc, char *buf, int pin) {
+#define TRUE 1
+#define FALSE 0
+
+ssize_t xgpio_readreg(int file_desc, char *buf, int pin) {
     ssize_t nbytes = read(file_desc,buf,sizeof(buf));
     // printf("no. of bytes read: %d\n", nbytes);
-    return;
+    return nbytes;
 }
 
-void xgpio_writereg(int file_desc, char *buf, int pin, int value) {
+ssize_t xgpio_writereg(int file_desc, char *buf, int pin, int value) {
     sprintf(buf, "(%d,%d)", pin, value);
     ssize_t nbytes = write(file_desc,buf,sizeof(buf));
-    return;
+    return nbytes;
 }
 
 int main() {
 
-    int fd = open("/proc/gpio_proc", O_RDWR);
+    int fd = open("/proc/gpio_driver", O_RDWR);
     if (fd < 0) {
-        perror("Failed to open file\n\n", fd);
+        perror("Failed to open file\n");
         return 1;
     }
 
@@ -30,16 +33,19 @@ int main() {
     char wr_buffer[1024];
     
     int led = 4;
+    ssize_t ret_chk;
 
-    while (true)
+    while (TRUE)
     {
         // xgpio_readreg(fd, rd_buffer, 8);
         // printf("sw[0]: %s", rd_buffer);
         // xgpio_readreg(fd, rd_buffer, 9);
         // printf("sw[0]: %s", rd_buffer);
-        xgpio_writereg(fd, wr_buffer, led, true);
+        ret chk = xgpio_writereg(fd, wr_buffer, led, TRUE);
+        printf("bytes written: %d\n", ret_chk);
         usleep(500000);
-        xgpio_writereg(fd, wr_buffer, led, false);
+        ret_chk = xgpio_writereg(fd, wr_buffer, led, FALSE);
+        printf("bytes written: %d\n", ret_chk);
         usleep(500000);
     }
 
