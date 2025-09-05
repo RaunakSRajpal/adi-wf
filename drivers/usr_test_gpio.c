631915b258d24e9e721a5d24e002fdec88d079db
RaunakSRajpal <raunaks.rajpal002@gmail.com>
2025-09-05 07:19:17 -0400

    add read reg test

--- drivers/usr_test_gpio.c
+++ drivers/usr_test_gpio.c
@@ -36,14 +36,17 @@ int main() {
     char wr_buffer[1024];
     
     int led = 4 + EMIO_BASE;
+    int sw0 = 8 + EMIO_BASE;
+    int sw1 = 9 + EMIO_BASE;
     ssize_t ret_chk;
 
     while (TRUE)
     {
-        // xgpio_readreg(fd, rd_buffer, 8);
-        // printf("sw[0]: %s", rd_buffer);
-        // xgpio_readreg(fd, rd_buffer, 9);
-        // printf("sw[0]: %s", rd_buffer);
+        xgpio_readreg(fd, rd_buffer, sw0);
+        printf("sw[0]: %s", rd_buffer);
+        xgpio_readreg(fd, rd_buffer, sw1);
+        printf("sw[1]: %s", rd_buffer);
+
         ret_chk = xgpio_writereg(fd, wr_buffer, led, TRUE);
         printf("bytes written: %d\n", ret_chk);
         usleep(500000);
