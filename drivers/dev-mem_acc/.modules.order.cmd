c81ad1aec02525804d6a87e79f3f3e6cafee305d
RaunakSRajpal <raunaks.rajpal002@gmail.com>
2025-09-29 11:34:01 -0400

    ADD lkm directory for memory accesses

---
+++ drivers/dev-mem_acc/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/raunak/wise-rsr/drivers/dev-mem_acc/modules.order := {   echo /home/raunak/wise-rsr/drivers/dev-mem_acc/devpl.o; :; } > /home/raunak/wise-rsr/drivers/dev-mem_acc/modules.order
