14326cc9bdde764f31d587ca3a651dd485371ac0
RaunakSRajpal <raunaks.rajpal002@gmail.com>
2025-09-29 11:31:09 -0400

    ADD  Test directory: update static methods

---
+++ drivers/test/test.c
@@ -0,0 +1,7 @@
+//#include "test.h"
+
+extern int var;
+
+int foobar() {
+    return var+10;
+}
---
+++ drivers/test/test.h
@@ -0,0 +1,12 @@
+#ifndef _TEST_H
+#define _TEST_H
+
+static int var=0;
+
+static inline int foo(int x) {
+    return x+10;
+}
+
+int foobar();
+
+#endif
